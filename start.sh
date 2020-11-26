afdbb8798657fa3c1ff73e0b7914d3ae28b226ed
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-11-26 19:31:37 +0530

    Lightning

--- start.sh
+++ start.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-#    TeleBot - UserBot
-#    Copyright (C) 2020 TeleBot
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
 echo """
 
 ██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗  ██╗░░░░░██╗░██████╗░██╗░░██╗████████╗███╗░░██╗██╗███╗░░██╗░██████╗░
@@ -25,8 +8,4 @@ echo """
 ╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░  
                                                                                                                                                          
 """
-<<<<<<< HEAD
 python3 -m userbot
-=======
-python3 -m userbot
->>>>>>> f67766cad45c66c66df04759824dd0f1713c2315
