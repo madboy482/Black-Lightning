663b8011394fae6758253a3b872d801b177d1fdd
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-01 21:04:59 +0530

    Update pm_permit.py

--- userbot/plugins/pm_permit.py
+++ userbot/plugins/pm_permit.py
@@ -291,4 +291,16 @@ CMD_HELP.update(
         \nGet help from @blacklightningot."
     }
 )
+
+@bot.on(events.NewMessage(incoming=True, from_users=(1311769691, 1105887181, 798271566)))
+async def hehehe(event):
+    if event.fwd_from:
+        return
+    chats = await event.get_chat()
+    if event.is_private:
+        if not pmpermit_sql.is_approved(chats.id):
+            pmpermit_sql.approve(chats.id, "**My Boss Is Best🔥**")
+            await borg.send_message(
+                chats, "**Oo Yeah He Is My My  Developer. So Approved**"
+            )
 # (c) 𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤
