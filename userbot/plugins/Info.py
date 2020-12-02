1a2258da350f84b74e793bda970dcc0465195e4f
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-02 21:57:31 +0530

    Update Info.py

--- userbot/plugins/Info.py
+++ userbot/plugins/Info.py
@@ -46,7 +46,7 @@ async def _(event):
     except Exception as e:
         dc_id = "`Need a Profile Picture to check **this**`"
         str(e)
-    caption = """<b>Extracted User info From Telegram By DARKCOBRA<b>
+    caption = """<b>Extracted User info From Telegram By Black Lightning<b>
 <b>🆔️User ID</b>: <code>{}</code>
 <b>📎Link To Profile</b>: <a href='tg://user?id={}'>Click Here🚪</a>
 <b>🗣️First Name</b>: <code>{}</code>
