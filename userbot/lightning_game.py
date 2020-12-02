c95f9c7c3f43b54e20df93591a77659b4aad1347
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-02 06:04:27 +0000

    pylint: auto fixes

--- userbot/lightning_game.py
+++ userbot/lightning_game.py
@@ -6,10 +6,11 @@ build by @Hack12R..
 Second game is a xogame
 Command:- .xogame .... By @Kraken_The_BadASS"""
 
-from telethon import events
 import asyncio
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from var import Var
+
+from telethon import events
+
+from userbot.utils import admin_cmd
 
 
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
@@ -22,12 +23,11 @@ async def _(event):
     if input_str == "game":
         await event.edit(input_str)
         animation_chars = [
-       
             "**Welcome To Lightning Repo Game**",
             "**Click The Gift As Fast As Possible**",
             "**Game Starts in 3**",
             "**Game Starts in 2**",
-            "**Game Starts in 1**",    
+            "**Game Starts in 1**",
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆[🎁](https://github.com/Anmol-dot283/Black-Lightning)🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆[🎁](https://github.com/Anmol-dot283/Black-Lightning)🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
@@ -36,15 +36,15 @@ async def _(event):
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n[🎁](https://github.com/Anmol-dot283/Black-Lightning)🎆🎇🎆🎇🎆🎇",
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇[🎁](https://github.com/Anmol-dot283/Black-Lightning)\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆",
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇\n🎇🎆🎇🎆🎇🎆🎇",
-            "**Game Over**"
- ]
+            "**Game Over**",
+        ]
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 14])
 
 
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from var import Var
+from userbot.utils import admin_cmd
+
 
 @borg.on(admin_cmd(pattern="xogame$"))
 async def gamez(event):
@@ -53,7 +53,7 @@ async def gamez(event):
     botusername = "@xobot"
     noob = "play"
     if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    tap = await bot.inline_query(botusername, noob) 
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
     await tap[0].click(event.chat_id)
     await event.delete()
