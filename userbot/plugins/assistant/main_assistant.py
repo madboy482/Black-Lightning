e44003681ade8b73937f7b097b884530e3454ba2
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-03 20:39:03 +0530

    Update main_assistant.py

--- userbot/plugins/assistant/main_assistant.py
+++ userbot/plugins/assistant/main_assistant.py
@@ -40,7 +40,7 @@ async def start(event):
     replied_user = await event.client(GetFullUserRequest(event.sender_id))
     firstname = replied_user.user.first_name
     vent = event.chat_id
-    starttext = f"Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Assistant Bot. \n\nMy [➤ Master](tg://user?id={bot.uid}) \nYou Can Talk/Contact My Master Using This Bot. \n\nIf You Want Your Own Assistant You Can Deploy From Button Below. \n\nPowered By [DarkCobra](https://t.me/Dark_cobra_support)"
+    starttext = f"Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Assistant Bot. \n\nMy [➤ Master](tg://user?id={bot.uid}) \nYou Can Talk/Contact My Master Using This Bot. \n\nIf You Want Your Own Assistant You Can Deploy From Button Below. \n\nPowered By [Black Lightning](https://t.me/blacklightningot)"
     if event.sender_id == bot.uid:
         await tgbot.send_message(
             vent,
@@ -66,7 +66,7 @@ async def start(event):
             link_preview=False,
             buttons=[
                 [custom.Button.inline("Deploy your Black Lightning", data="deploy")],
-                [Button.url("Help Me ❓", "https://t.me/blacklighttningot")],
+                [Button.url("Help Me ❓", "https://t.me/blacklightningot")],
             ],
         )
 
@@ -80,14 +80,9 @@ async def help(event):
     if event.query.user_id is not bot.uid:
         await tgbot.send_message(
             event.chat_id,
-            message="You Can Deploy DARKCOBRA In Heroku By Following Steps Bellow, You Can See Some Quick Guides On Support Channel Or On Your Own Assistant Bot. \nThank You For Contacting Me.",
+            message="You Can Deploy Black lihgning In Heroku By Following Steps Bellow, You Can See Some Quick Guides On Support Channel Or On Your Own Assistant Bot. \nThank You For Contacting Me.",
             buttons=[
-                [
-                    Button.url(
-                        "Deploy Tutorial 📺",
-                        "http://www.youtube.com/watch?v=-MbQO6kmP8o",
-                    )
-                ],
+                [Button.url("Deploy Tutorial 📺", "https://www.youtube.com/watch?v=GfZMqrCAqxI&t=339s")],
                 [Button.url("Need Help ❓", "https://t.me/blacklightningot")],
             ],
         )
