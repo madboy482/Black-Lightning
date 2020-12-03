331a5895eeea8ddd81e7d9339f4ac7d0e3a3d891
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-03 15:49:30 +0000

    pylint: auto fixes

--- userbot/plugins/_core.py
+++ userbot/plugins/_core.py
@@ -2,10 +2,10 @@ import asyncio
 import os
 from datetime import datetime
 from pathlib import Path
-from telethon.tl.types import InputMessagesFilterDocument
+
+from userbot import ALIVE_NAME, bot
 from userbot.utils import admin_cmd, load_module, remove_plugin
-from userbot import ALIVE_NAME
-from userbot import bot
+
 edit_or_reply = "reply"
 DELETE_TIMEOUT = 5
 thumb_image_path = "./resources/541200.png"
@@ -85,9 +85,7 @@ async def unload(event):
         await event.edit(f"Successfully unloaded {shortname}")
     except Exception as e:
         await event.edit(
-            "Successfully unloaded {shortname}\n{}".format(
-                shortname, str(e)
-            )
+            "Successfully unloaded {shortname}\n{}".format(shortname, str(e))
         )
 
 
