63c12b9e4c8074cfa2ed7ebe6057841dbeb40904
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-04 22:10:29 +0530

    Update remove.bg.py

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -75,7 +75,7 @@ async def _(event):
         end = datetime.now()
         ms = (end - start).seconds
         await event.edit(
-            "Removed image's Background in {} seconds, powered by @FridayOT".format(ms)
+            "Removed image's Background in {} seconds, powered by @blacklightningot".format(ms)
         )
     else:
         await event.edit(
