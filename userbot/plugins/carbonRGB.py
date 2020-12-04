576c48470603dd60288f760aaa6dc31b07fad22d
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-04 21:59:50 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -108,7 +108,7 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="Carbonised by [Dark-Cobra](https://github.com/DARK-COBRA/DARKCOBRA)",
+            caption="Carbonised by [Black Lightning(https://github.com/Anmol-dot283/Black-Lightning)",
             force_document=False,
             reply_to=e.message.reply_to_msg_id,
         )
