6bbeb65529a091d5b139a6ccd797124746aa0163
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-04 22:01:33 +0530

    Update cmds.py

--- userbot/plugins/cmds.py
+++ userbot/plugins/cmds.py
@@ -22,7 +22,7 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"**List of Plugins:**\n - {o}\n\n**HELP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All modules might not work directly. Visit__ @DARK_COBRA_SUPPORT __for assistance.__"
+    OUTPUT = f"**List of Plugins:**\n - {o}\n\n**HELP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All modules might not work directly. Visit__ @blacklightningot __for assistance.__"
     if len(OUTPUT) > 4095:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmd_list.text"
