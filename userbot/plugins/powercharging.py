771a63ff2901db997404a9d6fd2c1e2f61f2e5e3
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-04 03:38:11 +0000

    pylint: auto fixes

--- userbot/plugins/powercharging.py
+++ userbot/plugins/powercharging.py
@@ -4,43 +4,34 @@
 .wchar"""
 
 
+import asyncio
 
 from telethon import events
 
-from datetime import datetime
 
-from uniborg.util import admin_cmd
-
-import importlib.util
-
-import asyncio
-
-import random
-
-import importlib.util
-
-
-
-
-@borg.on(events.NewMessage(outgoing=True, pattern='^\.(q?w)char'))
-
+@borg.on(events.NewMessage(outgoing=True, pattern="^\.(q?w)char"))
 async def timer_blankx(e):
 
- txt=e.text[7:] + '\n\n`Tesla Wireless Charging (beta) Started...\nDevice Detected: Nokia 1100\nBattery Percentage:` '
+    txt = (
+        e.text[7:]
+        + "\n\n`Tesla Wireless Charging (beta) Started...\nDevice Detected: Nokia 1100\nBattery Percentage:` "
+    )
 
- j=10
+    j = 10
 
- k=j
+    k = j
 
- for j in range(j):
+    for j in range(j):
 
-  await e.edit(txt + str(k))
+        await e.edit(txt + str(k))
 
-  k=k+10
+        k = k + 10
 
-  await asyncio.sleep(1)
+        await asyncio.sleep(1)
 
- if e.pattern_match.group(1) == 'f':
+    if e.pattern_match.group(1) == "f":
 
-  await e.edit("`Tesla Wireless Charging (beta) Completed...\nDevice Detected: Nokia 1100 (Space Grey Varient)\nBattery Percentage:` [100%](https://telegra.ph/file/a45aa7450c8eefed599d9.mp4) ", link_preview=True)
-
+        await e.edit(
+            "`Tesla Wireless Charging (beta) Completed...\nDevice Detected: Nokia 1100 (Space Grey Varient)\nBattery Percentage:` [100%](https://telegra.ph/file/a45aa7450c8eefed599d9.mp4) ",
+            link_preview=True,
+        )
