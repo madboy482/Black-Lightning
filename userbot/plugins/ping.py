36de698208edaec1e78c760207a4cb791d019305
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-05 14:49:04 +0000

    pylint: auto fixes

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -2,7 +2,7 @@ import asyncio
 from datetime import datetime
 
 from .. import ALIVE_NAME, CMD_HELP
-from ..utils import admin_cmd, sudo_cmd, edit_or_reply
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
 
