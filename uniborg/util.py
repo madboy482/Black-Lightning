5fb3ae99b8074bcd356f4076a7c21659cd3498c0
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-07 20:56:36 +0530

    Update util.py

--- uniborg/util.py
+++ uniborg/util.py
@@ -1,8 +1,13 @@
 import functools
+import re
 import signal
 
+from telethon import events
 from telethon.tl.functions.messages import GetPeerDialogsRequest
-
+from userbot.thunderconfig import Config
+from var import Var
+from userbot import LOAD_PLUG
+from userbot import CMD_LIST
 
 async def is_read(borg, entity, message, is_out=None):
     """
@@ -11,7 +16,8 @@ async def is_read(borg, entity, message, is_out=None):
     """
     is_out = getattr(message, "out", is_out)
     if not isinstance(is_out, bool):
-        raise ValueError("Message was id but is_out not provided or not a bool")
+        raise ValueError(
+            "Message was id but is_out not provided or not a bool")
     message_id = getattr(message, "id", message)
     if not isinstance(message_id, int):
         raise ValueError("Failed to extract id from message")
@@ -34,14 +40,13 @@ async def get_target_self_message(borg, event):
 
 
 async def get_recent_self_message(borg, event):
-    async for message in borg.iter_messages(await event.get_input_chat(), limit=20):
+    async for message in borg.iter_messages(
+            await event.get_input_chat(), limit=20):
         if message.out:
             return message
 
-
 def _handle_timeout(signum, frame):
     raise TimeoutError("Execution took too long")
-
 
 def sync_timeout(seconds):
     def decorator(func):
@@ -54,18 +59,199 @@ def sync_timeout(seconds):
             finally:
                 signal.alarm(0)
             return r
-
         return wrapper
-
     return decorator
-
 
 async def send_replacement_message(event, *args, **kwargs):
     """
     Same as event.respond()
     but with reply_to already set to what this event is replying to
     """
-    if "reply_to" in kwargs:
+    if 'reply_to' in kwargs:
         raise ValueError("reply_to must not be provided")
-    kwargs["reply_to"] = event.message.reply_to_msg_id
+    kwargs['reply_to'] = event.message.reply_to_msg_id
     return await event.respond(*args, **kwargs)
+
+
+
+def sudo_cmd(pattern=None, **args):
+    args["func"] = lambda e: e.via_bot_id is None
+
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        else:
+            args["pattern"] = re.compile(sudo_hndlr + pattern)
+            cmd = sudo_hndlr + pattern
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = list(Var.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        args["allow_edited_updates"]
+        del args["allow_edited_updates"]
+
+    # check if the plugin should listen for outgoing 'messages'
+
+    return events.NewMessage(**args)
+
+
+async def edit_or_reply(event, text):
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        if reply_to:
+            return await reply_to.reply(text)
+        return await event.reply(text)
+    return await event.edit(text)
+
+
+async def eor(event, text):
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        if reply_to:
+            return await reply_to.reply(text)
+        return await event.reply(text)
+    return await event.edit(text)
+
+
+def admin_cmd(pattern=None, **args):
+    args["func"] = lambda e: e.via_bot_id is None
+
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        else:
+            args["pattern"] = re.compile(handler + pattern)
+            cmd = handler + pattern
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = list(Var.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        args["allow_edited_updates"]
+        del args["allow_edited_updates"]
+
+    # check if the plugin should listen for outgoing 'messages'
+
+    return events.NewMessage(**args)
+
+
+""" Userbot module for managing events.
+ One of the main components of the userbot. """
+
+
+
+def command(**args):
+    args["func"] = lambda e: e.via_bot_id is None
+
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    if 1 == 0:
+        return print("stupidity at its best")
+    else:
+        pattern = args.get("pattern", None)
+        allow_sudo = args.get("allow_sudo", None)
+        allow_edited_updates = args.get('allow_edited_updates', False)
+        args["incoming"] = args.get("incoming", False)
+        args["outgoing"] = True
+        if bool(args["incoming"]):
+            args["outgoing"] = False
+
+        try:
+            if pattern is not None and not pattern.startswith('(?i)'):
+                args['pattern'] = '(?i)' + pattern
+        except BaseException:
+            pass
+
+        reg = re.compile('(.*)')
+        if pattern is not None:
+            try:
+                cmd = re.search(reg, pattern)
+                try:
+                    cmd = cmd.group(1).replace(
+                        "$",
+                        "").replace(
+                        "\\",
+                        "").replace(
+                        "^",
+                        "")
+                except BaseException:
+                    pass
+
+                try:
+                    CMD_LIST[file_test].append(cmd)
+                except BaseException:
+                    CMD_LIST.update({file_test: [cmd]})
+            except BaseException:
+                pass
+
+        if allow_sudo:
+            args["from_users"] = list(Var.SUDO_USERS)
+            # Mutually exclusive with outgoing (can only set one of either).
+            args["incoming"] = True
+        del allow_sudo
+        try:
+            del args["allow_sudo"]
+        except BaseException:
+            pass
+
+        if "allow_edited_updates" in args:
+            del args['allow_edited_updates']
+
+        def decorator(func):
+            if allow_edited_updates:
+                bot.add_event_handler(func, events.MessageEdited(**args))
+            bot.add_event_handler(func, events.NewMessage(**args))
+            try:
+                LOAD_PLUG[file_test].append(func)
+            except BaseException:
+                LOAD_PLUG.update({file_test: [func]})
+            return func
+
+        return decorator
