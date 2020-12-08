9cef12f17a390b0bea205b2030689f6cb254c521
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-08 18:28:12 +0530

    Update _help.py

--- userbot/plugins/_help.py
+++ userbot/plugins/_help.py
@@ -2,6 +2,7 @@ import os
 
 from userbot import ALIVE_NAME, CMD_HELP, CMD_HNDLR, CMD_LIST
 from userbot.Config import Var
+from userbot import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤 User"
 CMD_HNDLR = Var.CMD_HNDLR
