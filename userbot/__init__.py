122a47e084fd0b930a2790122338e55a019f350c
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-08 12:49:56 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -15,7 +15,7 @@ from requests import get
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from userbot.Config import Config
+from userbot.thunderconfig import Config
 from var import Var
 
 from .function import thunderfunction as topfunc
