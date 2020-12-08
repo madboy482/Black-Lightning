77518dcfd045b3d28011cad41e565dcaba54d5a6
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-08 19:21:04 +0530

    Update zip_unzip.py

--- userbot/plugins/zip_unzip.py
+++ userbot/plugins/zip_unzip.py
@@ -8,7 +8,7 @@ import time
 import time as t
 import zipfile
 from datetime import datetime
-
+from userbot.Config import Var
 from userbot.utils import admin_cmd
 
 extracted = Var.TEMP_DOWNLOAD_DIRECTORY + "extracted/"
