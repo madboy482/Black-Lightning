ae952f7f5f65f17651db80d2791f4eaee4b00fd5
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-08 12:32:08 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,16 +1,17 @@
-import logging
-from pathlib import Path
-from sys import argv
-
-import telethon.utils
-from telethon import TelegramClient
-
+import glob
 from userbot import bot
-from userbot.Config import Config
+from sys import argv
+from telethon import TelegramClient
+from userbot.Config import Var
+from userbot.thunderconfig import Config
 from userbot.utils import load_module, start_assistant
-from var import Var
+from pathlib import Path
+import telethon.utils
+from userbot import CMD_HNDLR
 
-sed = logging.getLogger("Black Lightning")
+TELE = Var.PRIVATE_GROUP_ID
+BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
+LOAD_MYBOT = Var.LOAD_MYBOT
 
 
 async def add_bot(bot_token):
@@ -19,22 +20,43 @@ async def add_bot(bot_token):
     bot.uid = telethon.utils.get_peer_id(bot.me)
 
 
+async def startup_log_all_done():
+    try:
+        await bot.send_message(TELE, f"**Black Lightning has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**")
+    except BaseException:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
+
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished, no errors")
+        print("Starting Userbot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
     else:
         bot.start()
 
+path = 'userbot/plugins/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
 
-import glob
+print("Lightning has been deployed! ")
 
-path = "userbot/plugins/*.py"
+print("Setting up Lightning")
+path = "userbot/plugins/assistant/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -50,10 +72,11 @@ if Config.ENABLE_ASSISTANTBOT == "ENABLE":
             path1 = Path(f.name)
             shortname = path1.stem
             start_assistant(shortname.replace(".py", ""))
-    sed.info("Black Lightning Bot Have Been Installed Successfully !")
-else:
-    sed.info("Black Lightning Has Been Installed Sucessfully !")
-    sed.info("You Can Visit @lightnigsupport For Any Support Or Doubts")
+    print("Black Lightning set up completely!")
+
+print("Black Lightning set up - Level - Basic")
+print("Black Lightning has been fully deployed! Do Visit @lightningSupport")
+bot.loop.run_until_complete(startup_log_all_done())
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
