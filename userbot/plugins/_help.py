dea07b61bcb6cf2bb10c71e345e1c3a1ac780bd6
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-08 12:59:07 +0000

    pylint: auto fixes

--- userbot/plugins/_help.py
+++ userbot/plugins/_help.py
@@ -1,8 +1,7 @@
 import os
 
-from userbot import ALIVE_NAME, CMD_HELP, CMD_HNDLR, CMD_LIST
+from userbot import ALIVE_NAME, CMD_HELP, CMD_HNDLR, CMD_LIST, admin_cmd
 from userbot.Config import Var
-from userbot import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤 User"
 CMD_HNDLR = Var.CMD_HNDLR
