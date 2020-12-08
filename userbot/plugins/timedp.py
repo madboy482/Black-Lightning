6ab792e5d6c8bc24d93279ca7697d23d4589c44f
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-08 16:37:27 +0530

    Update timedp.py

--- userbot/plugins/timedp.py
+++ userbot/plugins/timedp.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
 from telethon.tl import functions
 
-from userbot.utils import Var
+from userbot.utils import admin_cmd
 
 FONT_FILE_TO_USE = "Fonts/digital.ttf"
 
