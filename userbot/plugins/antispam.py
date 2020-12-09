39bfc329d4055e616a83a3c1fb077ee85507455f
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-09 12:21:42 +0530

    Update antispam.py

--- userbot/plugins/antispam.py
+++ userbot/plugins/antispam.py
@@ -1,7 +1,7 @@
 from telethon.events import ChatAction
 
-from userbot import sclient
-from userbot.Config import Var
+from userbot import bot, sclient
+from userbot.thunderonfig import Config
 
 """Bans Spammers/Scammer At time Of Arrival 
 If You Add Him The Bot Won't Restrict."""
@@ -9,14 +9,17 @@ If You Add Him The Bot Won't Restrict."""
 
 @borg.on(ChatAction)
 async def ok(event):
-    if Var.ANTISPAM_FEATURE != "ENABLE":
+    juser = await event.get_user()
+    if Config.ANTISPAM_FEATURE != "ENABLE":
         return
     if event.user_joined:
-        juser = await event.get_user()
+        hmmyep = await borg.get_permissions(event.chat_id, bot.uid)
+        if not hmmyep.is_admin:
+            return
         user = sclient.is_banned(juser.id)
-        if user.banned == True:
+        if user:
             await event.reply(
-                f"**#ℓιgнтиιиg-ANTISPAM** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{user.reason}`"
+                f"**#Lightning-ANTISPAM** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{user.reason}`"
             )
             try:
                 await borg.edit_permissions(
