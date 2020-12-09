ad478bb809ba757efc5a23224097f3544ba315a1
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-09 06:10:11 +0000

    pylint: auto fixes

--- userbot/utils.py
+++ userbot/utils.py
@@ -88,8 +88,10 @@ def load_module(shortname):
     if shortname.startswith("__"):
         pass
     elif shortname.endswith("_"):
-        import userbot.utils
         import importlib
+
+        import userbot.utils
+
         path = Path(f"userbot/plugins/{shortname}.py")
         name = "userbot.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
@@ -97,8 +99,10 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         print("Successfully (re)imported " + shortname)
     else:
-        import userbot.utils
         import importlib
+
+        import userbot.utils
+
         path = Path(f"userbot/plugins/{shortname}.py")
         name = "userbot.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
