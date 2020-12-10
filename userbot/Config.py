5a68d56ed3b02a06d5cdc4342c9e7f6da87e3ff7
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-10 11:39:17 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -17,9 +17,9 @@ class Var(object):
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "1021716237").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "1311769691").split())
     WHITELIST_USERS = set(
-        int(x) for x in os.environ.get("WHITELIST_USERS", "1021716237").split()
+        int(x) for x in os.environ.get("WHITELIST_USERS", "1311769691").split()
     )
     BLACKLIST_USERS = set(int(x) for x in os.environ.get("BLACKLIST_USERS", "").split())
     DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "1311769691").split())
