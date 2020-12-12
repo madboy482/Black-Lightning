0028c8a94a799143d735d5ac3b79f38c0fcc99ce
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-12 09:12:28 +0000

    pylint: auto fixes

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -980,9 +980,7 @@ async def killing(killed):
     """ Dont Kill Too much -_-"""
     if not killed.text[0].isalpha() and killed.text[0] not in ("/", "#", "@", "!"):
         if await killed.get_reply_message():
-            await killed.edit(
-                "`Mar Gaya Betichod 😈......`\n" "#Sad_Reacts_Onli\n"
-            )
+            await killed.edit("`Mar Gaya Betichod 😈......`\n" "#Sad_Reacts_Onli\n")
 
 
 @register(outgoing=True, pattern="^.bt$")
