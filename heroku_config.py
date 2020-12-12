92e73ad93333e8d73f95ef160a030480f27fb60f
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-12 14:35:25 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -21,6 +21,7 @@ class Var(object):
     # Send .get_id in any channel to fill this value.
     PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", -100))
     PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", -100))
+    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", None)
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     NO_SONGS = bool(os.environ.get("NO_SONGS", False))
     DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
