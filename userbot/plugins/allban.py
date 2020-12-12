f12086cd2645ffe34a4966233406491f755f24c6
Anmol-dot283 <ypoxsisofficial@gmail.com>
2020-12-12 12:33:16 +0000

    pylint: auto fixes

--- userbot/plugins/allban.py
+++ userbot/plugins/allban.py
@@ -1,11 +1,13 @@
-#originally created by @danish_00
+# originally created by @danish_00
 
-#team cobra
+# team cobra
 from asyncio import sleep
-from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
+
 from telethon.tl.functions.channels import EditBannedRequest
-from userbot.utils import admin_cmd
+from telethon.tl.types import ChatBannedRights
+
 from userbot import bot
+from userbot.utils import admin_cmd
 
 
 @bot.on(admin_cmd(pattern=r"allban", outgoing=True))
@@ -15,17 +17,25 @@ async def testing(event):
     admin = nikal.admin_rights
     creator = nikal.creator
     if not admin and not creator:
-        await event.edit(" U Don't have sufficient permission 🧐 Alexa Iski Gand Maro 😑😑")
+        await event.edit(
+            " U Don't have sufficient permission 🧐 Alexa Iski Gand Maro 😑😑"
+        )
         return
-    await event.edit("Doing Nothing 🙃🙂")#Kang with Credits
-# for Dark_Cobra
+    await event.edit("Doing Nothing 🙃🙂")  # Kang with Credits
+    # for Dark_Cobra
     everyone = await event.client.get_participants(event.chat_id)
     for user in everyone:
         if user.id == chutiya.id:
             pass
         try:
-            await event.client(EditBannedRequest(event.chat_id, int(user.id), ChatBannedRights(until_date=None,view_messages=True)))
+            await event.client(
+                EditBannedRequest(
+                    event.chat_id,
+                    int(user.id),
+                    ChatBannedRights(until_date=None, view_messages=True),
+                )
+            )
         except Exception as e:
             await event.edit(str(e))
-        await sleep(.5)
+        await sleep(0.5)
     await event.edit("Nothing Happend here🙃🙂")
