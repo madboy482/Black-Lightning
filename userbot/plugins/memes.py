91bd297a7ba378f50e81d8c1b9cbafe879331e64
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-12 14:54:40 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -975,12 +975,12 @@ async def hoi(hello):
         await hello.edit(random.choice(HELLOSTR))
 
 
-@register(outgoing=True, pattern="^.abkill$")
+@register(outgoing=True, pattern="^.kill$")
 async def killing(killed):
     """ Dont Kill Too much -_-"""
     if not killed.text[0].isalpha() and killed.text[0] not in ("/", "#", "@", "!"):
         if await killed.get_reply_message():
-            await killed.edit("`Mar Gaya Betichod 😈......`\n" "#Sad_Reacts_Onli\n")
+            await killed.edit("`Mar Gaya Betichod 😈......`\n" "#betichod_dharti_pe_boj\n")
 
 
 @register(outgoing=True, pattern="^.bt$")
