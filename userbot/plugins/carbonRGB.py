d4035b4aa16703bb4450b73b228f14581279435e
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-13 17:22:14 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -108,7 +108,7 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="Carbonised by [Black Lightning(https://github.com/Anmol-dot283/Black-Lightning)",
+            caption="Carbonised by [Black Lightning(https://github.com/KeinShin/Black-Lightning)",
             force_document=False,
             reply_to=e.message.reply_to_msg_id,
         )
