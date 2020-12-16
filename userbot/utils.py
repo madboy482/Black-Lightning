684cd2a75f9cd60993253d35e8ed8b10f6b9dbbd
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-16 16:00:15 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -11,7 +11,7 @@ from userbot.Config import Var
 from userbot.thunderconfig import Config
 
 sedprint = logging.getLogger("PLUGINS")
-cmdhandler = Config.CMD_HNDLR if Config.CMD_HNDLR else "\."
+cmdhandler = Config.CMD_HNDLR if Config.CMD_HNDLR else "."
 bothandler = Config.BOT_HANDLER
 sudo_hndlr = Config.SUDO_HNDLR if Config.SUDO_HNDLR else "!"
 
@@ -151,26 +151,42 @@ def remove_plugin(shortname):
         raise ValueError
 
 
-def admin_cmd(pattern=None, **args):
+def admin_cmd(pattern=None, command=None, **args):
     args["func"] = lambda e: e.via_bot_id is None
-
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
     allow_sudo = args.get("allow_sudo", False)
-
     # get the pattern from the decorator
     if pattern is not None:
-        if pattern.startswith("\#"):
+        if pattern.startswith(r"\#"):
             # special fix for snip.py
             args["pattern"] = re.compile(pattern)
-        else:
-            args["pattern"] = re.compile(cmdhandler + pattern)
-            cmd = cmdhandler + pattern
+        elif pattern.startswith(r"^"):
+            args["pattern"] = re.compile(pattern)
+            cmd = pattern.replace("$", "").replace("^", "").replace("\\", "")
             try:
                 CMD_LIST[file_test].append(cmd)
-            except:
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        else:
+            if len(Config.CMD_HNDLR) == 2:
+                darkreg = "^" + Config.CMD_HNDLR
+                reg = Config.CMD_HNDLR[1]
+            elif len(Config.CMD_HNDLR) == 1:
+                darkreg = "^\\" + Config.CMD_HNDLR
+                reg = Config.CMD_HNDLR
+            args["pattern"] = re.compile(darkreg + pattern)
+            if command is not None:
+                cmd = reg + command
+            else:
+                cmd = (
+                    (reg + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
                 CMD_LIST.update({file_test: [cmd]})
 
     args["outgoing"] = True
@@ -186,13 +202,19 @@ def admin_cmd(pattern=None, **args):
         args["outgoing"] = True
 
     # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    # add blacklist chats, UB should not respond in these chats
     if "allow_edited_updates" in args and args["allow_edited_updates"]:
-        args["allow_edited_updates"]
         del args["allow_edited_updates"]
 
     # check if the plugin should listen for outgoing 'messages'
 
     return events.NewMessage(**args)
+
 
 
 """ Userbot module for managing events.
