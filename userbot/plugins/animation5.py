23c024c35a7985f2ce95fefda18c37d5bc80b48c
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-16 10:55:19 +0530

    Update animation5.py

--- userbot/plugins/animation5.py
+++ userbot/plugins/animation5.py
@@ -1,9 +1,9 @@
 import asyncio
 
-from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmdmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Lightning"
 
 
 @borg.on(admin_cmd(outgoing=True, pattern="loading( (.*)|$)"))
