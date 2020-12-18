ff86c7f4dfea68243ebca712a0bf5299fac91b10
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-18 22:10:27 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -159,7 +159,101 @@ async def spammer(e):
 
 
 @bot.on(admin_cmd(pattern="bigspam (.*)"))
-@bot.on(sudo_cmd(pattern="bigspam (.*)", allow_sudo=True))
+async def bigspam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
+        for i in range(1, counter):
+            await e.respond(spam_message)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP, "#BIGSPAM \n\n" "Bigspam was executed successfully"
+            )
+
+
+@bot.on(admin_cmd("wspam (.*)"))
+@bot.on(sudo_cmd(pattern="wspam (.*)", allow_sudo=True))
+async def tmeme(e):
+    wspam = str("".join(e.text.split(maxsplit=1)[1:]))
+    message = wspam.split()
+    await e.delete()
+    for word in message:
+        await e.respond(word)
+    if BOTLOG:
+        if e.is_private:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#WSPAM\n"
+                + f"Word Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with : `{message}`",
+            )
+        else:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#WSPAM\n"
+                + f"Word Spam was executed successfully in {e.chat.title}(`{e.chat_id}`) chat with : `{message}`",
+            )
+
+
+@bot.on(admin_cmd(pattern="mspam (.*)"))
+async def tiny_pic_spam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        text = message.split()
+        counter = int(text[1])
+        link = str(text[2])
+        for i in range(1, counter):
+            await e.client.send_file(e.chat_id, link)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP, "#PICSPAM \n\n" "PicSpam was executed successfully"
+            )
+
+
+@bot.on(admin_cmd("delayspam (.*)"))
+@bot.on(sudo_cmd(pattern="delayspam (.*)", allow_sudo=True))
+async def spammer(e):
+    if e.fwd_from:
+        return
+    input_str = "".join(e.text.split(maxsplit=1)[1:])
+    spamDelay = float(input_str.split(" ", 2)[0])
+    counter = int(input_str.split(" ", 2)[1])
+    spam_message = str(input_str.split(" ", 2)[2])
+    await e.delete()
+    for _ in range(counter):
+        await e.respond(spam_message)
+        await asyncio.sleep(spamDelay)
+    if BOTLOG:
+        if e.is_private:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#DELAYSPAM\n"
+                + f"Delay Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with {spamDelay}s Delay and {counter} times with : `{message}`",
+            )
+        else:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#DELAYCSPAM\n"
+                + f"Delay Spam was executed successfully in {e.chat.title}(`{e.chat_id}`) chat with {spamDelay}s Delay and {counter} times with: `{message}`",
+            )
+
+
+import asyncio
+import base64
+import os
+
+from telethon import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from userbot import CMD_HELP
+from userbot.plugins import BOTLOG, BOTLOG_CHATID
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+
+
+@bot.on(admin_cmd(pattern="spam (.*)"))
+@bot.on(sudo_cmd(pattern="spam (.*)", allow_sudo=True))
 async def spammer(e):
     if e.fwd_from:
         return
@@ -177,6 +271,8 @@ async def spammer(e):
         pass
     cat = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
     counter = int(cat[0])
+    if counter > 50:
+        return await edit_or_reply(e, "Use `.bigspam` for spam greater than 50")
     if len(cat) == 2:
         spam_message = str(("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)[1])
         await e.delete()
@@ -185,7 +281,7 @@ async def spammer(e):
                 await reply_to_id.reply(spam_message)
             else:
                 await e.client.send_message(e.chat_id, spam_message)
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(0.1)
         if BOTLOG:
             if e.is_private:
                 await e.client.send_message(
@@ -209,8 +305,12 @@ async def spammer(e):
         )
         await e.delete()
         if os.path.exists(downloaded_file_name):
+            sandy = None
             for _ in range(counter):
-                sandy = await e.client.send_file(e.chat_id, downloaded_file_name)
+                if sandy:
+                    sandy = await e.client.send_file(e.chat_id, sandy)
+                else:
+                    sandy = await e.client.send_file(e.chat_id, downloaded_file_name)
                 try:
                     await e.client(
                         functions.messages.SaveGifRequest(
@@ -224,7 +324,7 @@ async def spammer(e):
                     )
                 except:
                     pass
-                await asyncio.sleep(1)
+                await asyncio.sleep(0.5)
             if BOTLOG:
                 if e.is_private:
                     await e.client.send_message(
@@ -300,26 +400,18 @@ async def spammer(e):
         await edit_or_reply(e, "try again something went wrong or check `.info spam`")
 
 
-@bot.on(admin_cmd("cspam (.*)"))
-@bot.on(sudo_cmd(pattern="cspam (.*)", allow_sudo=True))
-async def tmeme(e):
-    cspam = str("".join(e.text.split(maxsplit=1)[1:]))
-    message = cspam.replace(" ", "")
-    await e.delete()
-    for letter in message:
-        await e.respond(letter)
-    if BOTLOG:
-        if e.is_private:
+@bot.on(admin_cmd(pattern="bigspam (.*)"))
+async def bigspam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
+        for i in range(1, counter):
+            await e.respond(spam_message)
+        await e.delete()
+        if LOGGER:
             await e.client.send_message(
-                BOTLOG_CHATID,
-                "#CSPAM\n"
-                + f"Letter Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with : `{message}`",
-            )
-        else:
-            await e.client.send_message(
-                BOTLOG_CHATID,
-                "#CSPAM\n"
-                + f"Letter Spam was executed successfully in {e.chat.title}(`{e.chat_id}`) chat with : `{message}`",
+                LOGGER_GROUP, "#BIGSPAM \n\n" "Bigspam was executed successfully"
             )
 
 
@@ -343,6 +435,22 @@ async def tmeme(e):
                 BOTLOG_CHATID,
                 "#WSPAM\n"
                 + f"Word Spam was executed successfully in {e.chat.title}(`{e.chat_id}`) chat with : `{message}`",
+            )
+
+
+@bot.on(admin_cmd(pattern="mspam (.*)"))
+async def tiny_pic_spam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        text = message.split()
+        counter = int(text[1])
+        link = str(text[2])
+        for i in range(1, counter):
+            await e.client.send_file(e.chat_id, link)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP, "#PICSPAM \n\n" "PicSpam was executed successfully"
             )
 
 
@@ -386,7 +494,7 @@ CMD_HELP.update(
         \n**Function : **__ Spam the text letter by letter.__\
         \n\n**Syntax : **`.wspam <text>`\
         \n**Function : **__ Spam the text word by word.__\
-        \n\n**Syntax : **`.delayspam <delay> <count> <text>`\
+        \n\n**Syntax : **`.mspam \ <count> >reply to media> \`\
         \n**Function : **__ .delayspam but with custom delay.__\
         \n\n\n**NOTE : Spam at your own risk !!**"
     }
