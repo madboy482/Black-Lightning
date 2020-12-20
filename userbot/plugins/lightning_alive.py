bdbdf3d06c1fb067a9d215d2d454dd7793578d8b
KeinShin <poxsisofficial@gmail.com>
2020-12-20 11:26:12 +0000

    pylint: auto fixes

--- userbot/plugins/lightning_alive.py
+++ userbot/plugins/lightning_alive.py
@@ -4,25 +4,16 @@
 # Kang with credits else gay...
 import asyncio
 import os
-import time
-from io import BytesIO
 
-import requests
-from PIL import Image
-
-from userbot import ALIVE_NAME, CMD_HELP, StartTime, topfunc
-from userbot.Config import Var
+from userbot import ALIVE_NAME
 from userbot.thunderconfig import Config
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
-
+from userbot.utils import admin_cmd, sudo_cmd
 
 LIGHTNING_IMAGE = os.environ.get("LIGHTNING_IMAGE", None)
 if LIGHTNING_IMAGE is None:
     LIGHTNING_IMG = "https://telegra.ph/file/c828d5c695b4cf95c814e.mp4"
 else:
     LIGHTNING_IMG = LIGHTNING_IMAGE
-
-
 
 
 version = "4.5"
@@ -61,36 +52,35 @@ pm_caption += "➾ `🔥Creator🔥` ☞ [ᴊᴏɪɴ](https://t.me/krish1303y)\n
 
 @borg.on(admin_cmd(pattern=r"alive"))
 @borg.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
-
 async def amireallyalive(yes):
-    chat = await yes.get_chat()
+    await yes.get_chat()
     global ghanti
     ghanti = borg.uid
-    on = await borg.send_file(yes.chat_id, file=file1,caption=pm_caption)
+    on = await borg.send_file(yes.chat_id, file=file1, caption=pm_caption)
 
     await asyncio.sleep(edit_time)
-    ok = await borg.edit_message(yes.chat_id, on, file=file2) 
+    ok = await borg.edit_message(yes.chat_id, on, file=file2)
 
     await asyncio.sleep(edit_time)
     ok2 = await borg.edit_message(yes.chat_id, ok, file=file3)
 
     await asyncio.sleep(edit_time)
     ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1)
-    
+
     await asyncio.sleep(edit_time)
     ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3)
-    
+
     await asyncio.sleep(edit_time)
     ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2)
-    
+
     await asyncio.sleep(edit_time)
     ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1)
-    
+
     await asyncio.sleep(edit_time)
     ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4)
 
     await yes.delete()
-    
+
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(yes.chat_id, PM_IMG,caption=pm_caption)
+    await borg.send_file(yes.chat_id, PM_IMG, caption=pm_caption)
     await yes.delete()
