801e2fa32cc6282778b470212f519a9d90b2a3bc
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-21 14:14:05 +0330

    Update pm_permit.py

--- userbot/plugins/pm_permit.py
+++ userbot/plugins/pm_permit.py
@@ -8,6 +8,8 @@ from telethon.tl.functions.users import GetFullUserRequest
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from userbot import ALIVE_NAME, CUSTOM_PMPERMIT
 from userbot.Configs import Config
+from userbot.thunderconfig import Config
+from userbot.utils import admin_cmd, command
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
