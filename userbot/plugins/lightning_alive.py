d55896be99b7287e5b61be2363b1af34c08194b3
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-21 12:33:55 +0530

    Update lightning_alive.py

--- userbot/plugins/lightning_alive.py
+++ userbot/plugins/lightning_alive.py
@@ -47,13 +47,13 @@ pm_caption += f"**𝕄𝕪 𝔹𝕠𝕤𝕤**            : {DEFAULTUSER}\n"
 pm_caption += "𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽        : 1.17.5\n"
 pm_caption += "𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽          : 3.9.0\n"
 pm_caption += (
-    "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴𝙻         : [ᴊᴏɪɴ](https://t.me/blacklightningot)\n"
+    "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴: [ᴊᴏɪɴ](https://t.me/blacklightningot)\n"
 )
 pm_caption += (
     "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿           : [ᴊᴏɪɴ](https://t.me/blacklightningot)\n"
 )
-pm_caption += "𝘓𝘐𝘚𝘌𝘕𝘊𝘌                  : [AGPL-3.0  ʟɪᴄᴇɴꜱᴇ](https://jenaatul8.wixsite.com/hellboi-atul)\n"
-pm_caption += "𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏
+pm_caption += "𝘓𝘐𝘚𝘌𝘕𝘊𝘌                  : [AGPL-3.0  ʟɪᴄᴇɴꜱᴇ](https://jenaatul8.wixsite.com/KeinShin)\n"
+pm_caption += "𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏:  [KeinShin](https://t.me//krish1303y)
 
 @borg.on(admin_cmd(pattern=r"alive"))
 @borg.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
