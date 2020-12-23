ca55ca1d8e28011d8780124ed654c66a977cc3ca
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-23 14:03:51 +0530

    Update lightning.py

--- userbot/plugins/lightning.py
+++ userbot/plugins/lightning.py
@@ -1,7 +1,11 @@
+import asyncio
+import html
 import os
 import re
+from math import ceil
 
 from telethon import Button, custom, events, functions
+from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot import ALIVE_NAME, CMD_HELP, CMD_LIST
 from userbot.plugins import inlinestats
