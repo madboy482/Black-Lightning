c01aea2bc327b03316bcf2416389d0ffa9d0f62f
KeinShin <poxsisofficial@gmail.com>
2020-12-24 09:12:05 +0000

    pylint: auto fixes

--- userbot/plugins/lawada.py
+++ userbot/plugins/lawada.py
@@ -143,8 +143,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     ],
                     [Button.url("Support", "https://t.me/lightningsupport")],
                 ],
-            
-              link_preview=False,
+                link_preview=False,
             )
         await event.answer([result] if result else None)
 
