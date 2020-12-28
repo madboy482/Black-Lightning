bf0e638baa261d3c899afab6545c2beaec3ad1f8
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-28 17:47:19 +0530

    Update help_menu.py

--- userbot/plugins/help_menu.py
+++ userbot/plugins/help_menu.py
@@ -1,14 +1,14 @@
 # Rewritten by @krish1303y
 
 from  userbot import CMD_LIST, ALIVE_NAME
-from userbot.utils import admin_cmd, lightning_command 
+from userbot.utils import admin_cmd, bot
 from telethon import  functions
 import asyncio
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Pls Go To Heroku Vars Then in  `ALIVE_NAME`place You Telegram `Username` "
 
-@lightning_command(pattern="^.help ?(.*)")
+@bot.on(admin_cmd(pattern="help ?(.*)"))
 async def lightning_cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         lightning_userbot_name = Var.TG_BOT_USER_NAME_BF_HER
