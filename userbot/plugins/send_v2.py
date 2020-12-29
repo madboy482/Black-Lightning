f3ae849790df12437d91f7a74793eba485f7cfa7
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-29 20:03:01 +0530

    Update send_v2.py

--- userbot/plugins/send_v2.py
+++ userbot/plugins/send_v2.py
@@ -1,4 +1,5 @@
-# Changes Fixed By @krish1303y  
+# Changes Fixed By @krish1303y
+# Credits To Team DC
 import asyncio
 from userbot import ALIVE_NAME
 import os
@@ -38,6 +39,6 @@ async def send(event):
             f"**==> Plugin name:** 🗡`{input_str}`🗡\n**==> Uploaded in {time_taken_in_ms} seconds only.**\n**==> Uploaded by:** [{DEFAULTUSER}](tg://user?id={lightning_hehehe})\n",
         )
         await asyncio.sleep(DELETE_TIMEOUT)
-        await event.edit("__sent!!__") #only italic if loaded markdown else it doesn't look gr8
+        await event.edit("__Done See The Taged Msg!!__") #only italic if loaded markdown else it doesn't look gr8
     else:
         await eor(event, f"**404**: {DEFAULTUSER}\n __No File Is Name As__ 🗡{input_str}🗡\n\nPlease Try With Right Name😔")
