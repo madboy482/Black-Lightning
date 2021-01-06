b9799ab53a0f825fa3fe7ef8bd0ffe0b530e1f2e
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-06 22:38:47 +0530

    Done Heheh

--- userbot/plugins/_inlineee.py
+++ userbot/plugins/_inlineee.py
@@ -349,8 +349,13 @@ async def lightning_pugins_query_hndlr(lightning):
         else:
             for light_pulu_name in CMD_LIST[light_pulu_name]:
                 lightning_help_strin = f"**🔺 NAME 🔺 :** `{light_pulu_name}` \n\n{CMD_LIST[light_pulu_name]}"
+                lightning_is_best = lightning_help_strin 
+                lightning_is_best += "\n\n**In Case Any Problem @lightningsupport** ".format(light_pulu_name)
     except BaseException:
          pass
+    lightning_help_strin = f"**🔺 NAME 🔺 :** `{light_pulu_name}` \n\n{CMD_LIST[light_pulu_name]}"
+    lightning_is_best = lightning_help_strin 
+    lightning_is_best += "\n\n**In Case Any Problem @lightningsupport** ".format(light_pulu_name)    
     if len(lightning_is_best) >= 4096:
           keinshin = "`Wait.( ͡🔥 ͜ʖ ͡🔥)`"
           await lightning.answer(keinshin, cache_time=0, alert=True)
