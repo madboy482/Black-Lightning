9595ebd44ad0aab933018025d436c44bef054f16
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-06 12:45:34 +0530

    Update _inlineee.py

--- userbot/plugins/_inlineee.py
+++ userbot/plugins/_inlineee.py
@@ -615,7 +615,7 @@ async def lightning_pugins_query_hndlr(lightning):
 
     
 def lightnings_menu_for_help(b_lac_krish, lightning_plugs, lightning_lol):
-    lightning_no_rows = 12
+    lightning_no_rows = 10
     lightning_no_coulmns = 3
     lightning_plugins = []
     for p in lightning_plugs:
