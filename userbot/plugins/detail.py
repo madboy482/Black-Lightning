e5e60ef053a44813a0a83ee29d515e73434a7115
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-07 16:46:49 +0530

    Update detail.py

--- userbot/plugins/detail.py
+++ userbot/plugins/detail.py
@@ -1,31 +1,8 @@
 from userbot import CMD_HELP, bot
-from userbot.utils import friday_on_cmd, sudo_cmd
-
-
-@bot.on(sudo_cmd(pattern="ahelp ?(.*)", allow_sudo=True))
-@bot.on(friday_on_cmd(pattern="ahelp ?(.*)"))
-async def _(event):
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await event.edit(f"Here is some help for the {CMD_HELP[args]}")
-        else:
-            await event.edit(
-                f"Help string for {args} not found! Type `.help` to see valid module names."
-            )
-    else:
-        string = ""
-        for i in CMD_HELP.values():
-            string += f"`{str(i[0])}`, "
-        string = string[:-2]
-        await event.edit(
-            "Please specify which module you want help for!\n\n" f"{string}"
-        )from userbot import CMD_HELP, bot
-from userbot.utils import admin_cmd, sudo_cmd
-import asyncio
+from userbot.utils import lightning_cmd, sudo_cmd
 
 @bot.on(sudo_cmd(pattern="detailed ?(.*)", allow_sudo=True))
-@bot.on(admin_cmd(pattern="detailed ?(.*)"))
+@bot.on(lightning_cmd(pattern="detailed ?(.*)"))
 async def _(event):
     help_plugs = event.pattern_match.group(1).lower()
     if help_plugs:
