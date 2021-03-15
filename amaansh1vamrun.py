eb01fd5a344e2c4c03213ccb84d087bb7df71216
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2021-03-15 20:57:18 +0530

    Update amaansh1vamrun.py

--- amaansh1vamrun.py
+++ amaansh1vamrun.py
@@ -1,3 +1,3 @@
- 
+#To solbe suspension issue #By sh1vam
 import os
 os.system("git clone https://github.com/KeinShin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
