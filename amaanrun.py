a0690f2650a5344dc271dce521775d1a5e31659e
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-03-15 15:41:48 +0530

    Update amaanrun.py

--- amaanrun.py
+++ amaanrun.py
@@ -1,1 +1,3 @@
-
+ 
+import os
+os.system("git clone https://github.com/KeinShin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot")
